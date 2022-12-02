d0b19e8d7b87b6fb746956a4331162fc2fedbec5
Gabriel Garcia <89267342+gabito1966@users.noreply.github.com>
2022-12-02 17:02:35 -0300

    Clase24

--- src/app/caracteristicas-empleado-c/caracteristicas-empleado-c.component.ts
+++ src/app/caracteristicas-empleado-c/caracteristicas-empleado-c.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-//import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-caracteristicas-empleado-c',
@@ -13,13 +13,13 @@ export class CaracteristicasEmpleadoCComponent implements OnInit{
 
   @Output() caracteristicasEmpleados = new EventEmitter<string>();
 
-  //constructor(private miServicio:ServicioEmpleadosService){}
+  constructor(private miServicio:ServicioEmpleadosService){}
 
   
 
   agregaCaracteristicas(value: string) {
-   // this.miServicio.muestraMensaje(value);
-    //this.caracteristicasEmpleados.emit(value);
+   this.miServicio.muestraMensaje(value);
+    this.caracteristicasEmpleados.emit(value);
   }
 
 }
